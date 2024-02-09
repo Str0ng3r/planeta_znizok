704fa1e6c6d1c76fe933bdfcc449f2f4a816adc8
Maks <116167012+Str0ng3r@users.noreply.github.com>
2024-02-09 19:29:38 +0100

    header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,31 +1,28 @@
-import styles from '../header/header.module.css'
-
+import styles from "../header/header.module.css";
 
 export const Header = () => {
-
-    return (
-<header>
-    <div className={styles.cont}>
+  return (
+    <header>
+      <div className={styles.cont}>
         <img src="" alt="" />
         <nav>
-            <ul className={styles.nav_list}>
-                <li className={styles.nav_link}>Студенту</li>
-                <li className={styles.nav_link}>Факультети</li>
-                <li className={styles.nav_link}>Підрозділи</li>
-                <li className={styles.nav_link}>Міжнародна діяльність</li>
-                <li className={styles.nav_link}>Управління</li>
-                <li className={styles.nav_link}>Університет</li>
-                <li className={styles.nav_link}>Наука</li>
-            </ul>
+          <ul className={styles.nav_list}>
+            <li className={styles.nav_link}>Студенту</li>
+            <li className={styles.nav_link}>Факультети</li>
+            <li className={styles.nav_link}>Підрозділи</li>
+            <li className={styles.nav_link}>Міжнародна діяльність</li>
+            <li className={styles.nav_link}>Управління</li>
+            <li className={styles.nav_link}>Університет</li>
+            <li className={styles.nav_link}>Наука</li>
+          </ul>
         </nav>
         <div className={styles.social}>
-
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
         </div>
         <button className={styles.button_call}>Зв'язок з нами</button>
-    </div>
-</header>
-    )
-}
-
-
-
+      </div>
+    </header>
+  );
+};
