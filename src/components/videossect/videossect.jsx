b1919de8bec366d900e2a4bbfbb6e679de062a74
Maks <116167012+Str0ng3r@users.noreply.github.com>
2024-02-14 03:53:51 +0100

    videossection

---
+++ src/components/videossect/videossect.jsx
@@ -0,0 +1,14 @@
+import styles from './videossect.module.css'
+export const Videossect = () => {
+    return (
+        <section>
+            <div className={styles.cont}>
+<h2>Правила безпеки у разі надзвичайних ситуацій</h2>
+<h4>Не забувай притримуватися правил!</h4>
+<div className={styles.wrap_videos}>
+    
+</div>
+            </div>
+        </section>
+    )
+}
